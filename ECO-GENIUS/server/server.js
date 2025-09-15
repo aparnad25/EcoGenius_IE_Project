022748d38c63c95b5754bee56b7b2a7b8eb8133e
YinfangSU <suyinfang678@outlook.com>
2025-09-15 20:49:52 +1000

    Updated documentation of serveer.js

--- ECO-GENIUS/server/server.js
+++ ECO-GENIUS/server/server.js
@@ -7,7 +7,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-//const upload = multer({ dest: 'uploads/' });
+
+// ========= Multer for form-data upload =========
+const upload = multer({ dest: 'uploads/' });   // Temporary storage for uploaded files
 
 // Configure Cloudinary
 cloudinary.config({
@@ -16,12 +18,14 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// ========= Middleware =========
 app.use(cors());
 //app.use(express.json());
 app.use(express.json({ limit: '10mb' })); // allow big file base64
 
+// ========= Routes =========
 
-// Upload endpoint
+// 1. Form-data upload, this is only for local testing
 app.post('/api/upload', upload.single('file'), async (req, res) => {
   try {
     const result = await cloudinary.uploader.upload(req.file.path, {
@@ -36,7 +40,7 @@ app.post('/api/upload', upload.single('file'), async (req, res) => {
   }
 });
 
-// Upload endpoint (Base64 version)
+// 2. Base64 upload (for debugging), this is for AWS Lambda
 app.post('/api/upload', async (req, res) => {
   try {
     const { file } = req.body; // front end passing base64
@@ -57,6 +61,7 @@ app.post('/api/upload', async (req, res) => {
 });
 
 
+// ========= Start server =========
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
